e3f362a38927bf72e28ddddda02b084658bbe4cf
Kouhei Sutou <kou@clear-code.com>
2016-08-21 16:35:14 +0900

    Use twitter gem instead of twitter-stream

--- lib/rabbiter.rb
+++ lib/rabbiter.rb
@@ -18,8 +18,7 @@ require "shellwords"
 require "pathname"
 require "yaml"
 
-require "gio2"
-require "twitter/json_stream"
+require "twitter"
 require "twitter_oauth"
 
 require "rabbit/utils"
@@ -52,8 +51,6 @@ module Rabbiter
       setup_access_token unless @config_file_path.exist?
       oauth_access_parameters = YAML.load(@config_file_path.read)
       @oauth_parameters = {
-        :consumer_key => CONSUMER_KEY,
-        :consumer_secret => CONSUMER_SECRET,
         :access_key => oauth_access_parameters[:access_token],
         :access_secret => oauth_access_parameters[:access_secret],
       }
@@ -71,40 +68,18 @@ module Rabbiter
       return if @oauth_parameters.nil?
 
       stream_options = {
-        :oauth => @oauth_parameters,
-        :user_agent => "Rabbiter #{Rabbiter::VERSION}",
-        :method => "POST",
-        :filters => filters,
+        :access_token        => @oauth_parameters[:access_key],
+        :access_token_secret => @oauth_parameters[:access_secret],
+        :consumer_key        => CONSUMER_KEY,
+        :consumer_secret     => CONSUMER_SECRET,
+        :user_agent          => "Rabbiter #{Rabbiter::VERSION}",
       }
-      @stream = ::Twitter::JSONStream.allocate
-      @stream.send(:initialize, stream_options)
-      @stream.send(:reset_state)
-      @connection = GLibConnection.new(@logger, @stream)
-
-      @stream.each_item do |item|
-        status = JSON.parse(item)
+      @client = ::Twitter::Streaming::Client.new(stream_options)
+      @client.filter(:track => filters.join(",")) do |status|
         @listeners.each do |listener|
           listener.call(status)
         end
       end
-
-      @stream.on_error do |message|
-        @logger.error("[twitter] #{message}")
-      end
-
-      @stream.on_reconnect do |timeout, n_retries|
-        format = _("%{timeout} seconds (%{n_retries})")
-        message = format % {:timeout => timeout, :n_retries => retries}
-        @logger.info("[twitter][reconnect] #{message}")
-      end
-
-      @stream.on_max_reconnects do |timeout, n_retries|
-        format = _("Failed after %{n_retries} failed reconnects")
-        message = format % {:n_retries => retries}
-        @logger.info("[twitter][max-reconnects] #{message}")
-      end
-
-      @connection.connect
     end
 
     private
@@ -139,125 +114,6 @@ module Rabbiter
         Gtk.show_uri(uri)
       rescue GLib::Error
         @logger.warning("[twitter][show-uri] #{$!}")
-      end
-    end
-
-    class GLibConnection
-      def initialize(logger, handler)
-        @logger = logger
-        @handler = handler
-        @options = @handler.instance_variable_get("@options")
-        @client = nil
-        @connection = nil
-        @source_ids = []
-      end
-
-      def connect
-        close
-        @client = Gio::SocketClient.new
-        @client.tls = @options[:ssl]
-        @client.tls_validation_flags = :validate_all
-        if Rabbit::Utils.windows?
-          @client.tls_validation_flags -= :unknown_ca
-        end
-        @connection = @client.connect_to_host(@options[:host], @options[:port])
-        @input = @connection.input_stream
-        @output = @connection.output_stream
-
-        reader_source = @input.create_source do |stream|
-          @logger.debug("[twitter][read][start]")
-          begin
-            data = @input.read_nonblocking(8192)
-          rescue Gio::IOError::WouldBlock
-              data = ""
-          rescue => error
-            @logger.debug("[twitter][read][error] #{error}")
-            @handler.send(:receive_error, "#{error.class}: #{error}")
-            data = nil
-          end
-          if data
-            @logger.debug("[twitter][read][done] #{data.bytesize}")
-            @handler.receive_data(data) unless data.empty?
-            true
-          else
-            false
-          end
-        end
-        @source_ids << reader_source.attach
-
-        @handler.extend(GLibAdapter)
-        @handler.connection = self
-        @handler.connection_completed
-      end
-
-      def send_data(data)
-        rest = data.bytesize
-        writer_source = @output.create_source do |stream|
-          if rest.zero?
-            @logger.debug("[twitter][flush][start]")
-            @output.flush
-            @logger.debug("[twitter][flush][done]")
-            @source_ids.reject! {|id| id == writer_source.id}
-            false
-          else
-            @logger.debug("[twitter][write][start]")
-            written_size = @output.write_nonblocking(data)
-            @logger.debug("[twitter][write][done] #{written_size}")
-            rest -= written_size
-            data[0, written_size] = ""
-            true
-          end
-        end
-        @source_ids << writer_source.attach
-      end
-
-      def close
-        return if @client.nil?
-        @source_ids.reject! do |id|
-          GLib::Source.remove(id)
-          true
-        end
-        @input = nil
-        @output = nil
-        @connection = nil
-        @client = nil
-      end
-
-      def reconnect(options={})
-        close
-        after = options[:after] || 0
-        if after.zero?
-          connect
-        else
-          id = GLib::Timeout.add(after) do
-            connect
-            false
-          end
-          @source_ids << id
-        end
-      end
-    end
-
-    module GLibAdapter
-      attr_accessor :connection
-      def start_tls
-      end
-
-      def send_data(data)
-        @connection.send_data(data)
-      end
-
-      def reconnect_after(timeout)
-        @reconnect_callback.call(timeout, @reconnect_retries) if @reconnect_callback
-        @connection.reconnect(:after => timeout)
-      end
-
-      def reconnect(server, port)
-        @connection.reconnect
-      end
-
-      def close_connection
-        @connection.close
       end
     end
   end
--- rabbiter.gemspec
+++ rabbiter.gemspec
@@ -48,7 +48,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency("rabbit", ">= 2.0.0")
   spec.add_runtime_dependency("gio2", ">= 2.1.1")
   spec.add_runtime_dependency("twitter_oauth")
-  spec.add_runtime_dependency("twitter-stream", ">= 0.1.16")
+  spec.add_runtime_dependency("twitter")
 
   spec.add_development_dependency("rake")
   spec.add_development_dependency("bundler")
